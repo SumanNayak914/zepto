f8daa118acf60997d29a6eb0e9b8b17a13267568
aloknaik <aloknaik016261@gmail.com>
2025-08-25 16:12:44 +0530

    fixed bottom navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import React from 'react'
-import Category from './pages/Category';
-import DealStore from './pages/DealStore';
-import Cart from './pages/Cart';
-import BottomNav from './components/BottomNav';
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Category from "./pages/Category";
+import DealStore from "./pages/DealStore";
+import Cart from "./pages/Cart";
+import BottomNav from "./components/BottomNav";
 
 const App = () => {
+  const location = useLocation();
   return (
     <>
-       <BrowserRouter>
-     <Header/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/category" element={<Category/>} />
-      <Route path="/dealstore" element={<DealStore/>} />
-      <Route path="/cart" element={<Cart/>} />
-      
-    </Routes>
-  <Footer/>
-  <BottomNav/>
-  </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/dealstore" element={<DealStore />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+      {location.pathname === "/" && <BottomNav />}
     </>
-  )
-}
+  );
+};
 
 export default App;
